95926c52609c9145cd278818633034a28a3a264f
MartinBG <MartinBG@abv.bg>
2018-09-29 05:42:58 +0300

    done task #4

--- 04. Arrays and Matrices/Exercise/04. Rotate Array.js
+++ 04. Arrays and Matrices/Exercise/04. Rotate Array.js
@@ -1,1 +1,23 @@
-//TODO
+function rotateArray(input) {
+  let arr = [].concat(input);
+  const rotations = +arr.pop() % (input.length - 1);
+  const rotated = arr.splice(arr.length - rotations);
+  let result = rotated.concat(arr);
+  console.log(result.join(' '));
+}
+
+rotateArray([
+  '1',
+  '2',
+  '3',
+  '4',
+  '2',
+]);
+
+rotateArray([
+  'Banana',
+  'Orange',
+  'Coconut',
+  'Apple',
+  '15',
+]);
