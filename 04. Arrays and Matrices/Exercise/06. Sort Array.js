0735bda65fbe627caa06ec287152d20450ccde26
MartinBG <MartinBG@abv.bg>
2018-09-29 05:56:18 +0300

    done task #6

--- 04. Arrays and Matrices/Exercise/06. Sort Array.js
+++ 04. Arrays and Matrices/Exercise/06. Sort Array.js
@@ -1,1 +1,27 @@
-//TODO
+function sortArray(input) {
+  console.log([].concat(input)
+    .sort((a, b) => a.length - b.length || a > b)
+    .join('\n')
+  );
+}
+
+sortArray([
+  'alpha',
+  'beta',
+  'gamma',
+]);
+
+sortArray([
+  'Isacc',
+  'Theodor',
+  'Jack',
+  'Harrison',
+  'George',
+]);
+
+sortArray([
+  'test',
+  'Deny',
+  'omen',
+  'Default',
+]);
