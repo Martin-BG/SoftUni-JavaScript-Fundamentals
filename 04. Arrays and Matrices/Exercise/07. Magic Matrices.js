b7ad13d3bda5d2ec3c8036480be9d64e206ba071
MartinBG <MartinBG@abv.bg>
2018-09-29 06:18:33 +0300

    done task #7

--- 04. Arrays and Matrices/Exercise/07. Magic Matrices.js
+++ 04. Arrays and Matrices/Exercise/07. Magic Matrices.js
@@ -1,1 +1,39 @@
-//TODO
+function magicMatrices(input) {
+  const sum = input[0].reduce((a, b) => a + b);
+
+  for (let i = 1; i < input.length; i++) {
+    if (sum !== input[i].reduce((a, b) => a + b)) {
+      return false;
+    }
+  }
+
+  for (let col = 0; col < input[0].length; col++) {
+    let currentSum = 0;
+    for (let row = 0; row < input.length; row++) {
+      currentSum += input[row][col];
+    }
+    if (sum !== currentSum) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+console.log(magicMatrices([
+  [4, 5, 6],
+  [6, 5, 4],
+  [5, 5, 5],
+]));
+
+console.log(magicMatrices([
+  [11, 32, 45],
+  [21, 0, 1],
+  [21, 1, 1],
+]));
+
+console.log(magicMatrices([
+  [1, 0, 0],
+  [0, 0, 1],
+  [0, 1, 0],
+]));
