7063e70234855e0fa355db47ebc715ae165e0070
MartinBG <MartinBG@abv.bg>
2018-09-29 05:50:53 +0300

    done task #5

--- 04. Arrays and Matrices/Exercise/05. Extract Increasing Subsequence from Array.js
+++ 04. Arrays and Matrices/Exercise/05. Extract Increasing Subsequence from Array.js
@@ -1,1 +1,18 @@
-//TODO
+function extractIncreasingNumbers(input) {
+  let biggest = +input[0];
+  console.log(input
+    .map(x => +x)
+    .filter(n => {
+      if (n >= biggest) {
+        biggest = n;
+        return true;
+      }
+      return false;
+    })
+    .join('\n')
+  );
+}
+
+extractIncreasingNumbers([1, 3, 8, 4, 10, 12, 3, 2, 24]);
+extractIncreasingNumbers([1, 2, 3, 4]);
+extractIncreasingNumbers([20, 3, 2, 15, 6, 1]);
