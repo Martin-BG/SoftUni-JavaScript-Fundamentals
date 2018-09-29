66d8818c7311bf8a5ef4cae7c1a40b02aad7d2be
MartinBG <MartinBG@abv.bg>
2018-09-29 05:01:13 +0300

    done exercise #1

--- 04. Arrays and Matrices/Exercise/01. Print Array with Given Delimiter.js
+++ 04. Arrays and Matrices/Exercise/01. Print Array with Given Delimiter.js
@@ -1,1 +1,25 @@
-//TODO
+function printArrayWithGivenDelimiter(arr) {
+  console.log(arr
+    .slice(0, arr.length - 1)
+    .join(arr[arr.length - 1])
+  );
+}
+
+printArrayWithGivenDelimiter([
+  'One',
+  'Two',
+  'Three',
+  'Four',
+  'Five',
+  '-'
+]);
+
+printArrayWithGivenDelimiter([
+  'How about no?',
+  'I',
+  'will',
+  'not',
+  'do',
+  'it!',
+  '_'
+]);
