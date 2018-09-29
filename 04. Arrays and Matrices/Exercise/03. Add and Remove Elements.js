1b51094c16b6df7bc2eeba473967d9a5736eae92
MartinBG <MartinBG@abv.bg>
2018-09-29 05:19:54 +0300

    done task #3

--- 04. Arrays and Matrices/Exercise/03. Add and Remove Elements.js
+++ 04. Arrays and Matrices/Exercise/03. Add and Remove Elements.js
@@ -1,1 +1,34 @@
-//TODO
+function addAndRemoveElements(commands) {
+  let elements = [];
+  let step = 0;
+  commands.forEach((command) => {
+    ++step;
+    if (command === 'add') {
+      elements.push(step);
+    } else if (command === 'remove' && elements.length > 0) {
+      elements.pop();
+    }
+  });
+  console.log(elements.length > 0 ? elements.join('\n') : 'Empty');
+}
+
+addAndRemoveElements([
+  'add',
+  'add',
+  'add',
+  'add',
+]);
+
+addAndRemoveElements([
+  'add',
+  'add',
+  'remove',
+  'add',
+  'add',
+]);
+
+addAndRemoveElements([
+  'remove',
+  'remove',
+  'remove',
+]);
