a7f539fc239ab73595429726f4e2c48c900f68a0
MartinBG <MartinBG@abv.bg>
2018-09-29 05:10:24 +0300

    done task #2

--- 04. Arrays and Matrices/Exercise/02. Print every N-th Element from an Array.js
+++ 04. Arrays and Matrices/Exercise/02. Print every N-th Element from an Array.js
@@ -1,1 +1,33 @@
-//TODO
+function printEveryNthElementFromArray(input) {
+  const step = +input[input.length - 1];
+  console.log(input
+    .filter(((value, index) => index % step === 0 && index < input.length - 1))
+    .join('\n')
+  );
+}
+
+printEveryNthElementFromArray([
+  '5',
+  '20',
+  '31',
+  '4',
+  '20',
+  '2',
+]);
+
+printEveryNthElementFromArray([
+  'dsa',
+  'asd',
+  'test',
+  'tset',
+  '2',
+]);
+
+printEveryNthElementFromArray([
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+]);
