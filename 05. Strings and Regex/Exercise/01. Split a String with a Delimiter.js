abc1e94cf6ced98118a8828b0981884ad5363065
MartinBG <MartinBG@abv.bg>
2018-10-07 01:21:27 +0300

    done exercise #1

--- 05. Strings and Regex/Exercise/01. Split a String with a Delimiter.js
+++ 05. Strings and Regex/Exercise/01. Split a String with a Delimiter.js
@@ -1,1 +1,8 @@
-//TODO
+function splitByDelimiter(text, delimiter) {
+  console.log(text.split(delimiter).join('\n'));
+}
+
+splitByDelimiter('One-Two-Three-Four-Five', '-');
+
+splitByDelimiter('http://platform.softuni.bg', '.');
+
