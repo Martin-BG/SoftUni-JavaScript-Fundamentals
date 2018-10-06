15a726842afc9a36a87fbbf8dd39233ef22142c8
MartinBG <MartinBG@abv.bg>
2018-10-07 02:43:13 +0300

    done exercise #8

--- 05. Strings and Regex/Exercise/08. Word Occurences.js
+++ 05. Strings and Regex/Exercise/08. Word Occurences.js
@@ -1,1 +1,10 @@
-//TODO
+function wordOccurences(text, word) {
+  const matches = text.match(new RegExp(`\\b${word}\\b`, 'gi')) || [];
+  console.log(matches.length);
+}
+
+wordOccurences('The waterfall was so high, that the child couldn’t see its peak.', 'the');
+
+wordOccurences('How do you plan on achieving that? How? How can you even think of that?', 'how');
+
+wordOccurences('There was one. Therefore I bought it. I wouldn’t buy it otherwise.', 'there');
