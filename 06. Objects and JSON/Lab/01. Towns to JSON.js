d775a6fcd91db7da6b61c6d78e4f27b6fac9c020
MartinBG <MartinBG@abv.bg>
2018-10-06 23:39:11 +0300

    done task #1

--- 06. Objects and JSON/Lab/01. Towns to JSON.js
+++ 06. Objects and JSON/Lab/01. Towns to JSON.js
@@ -1,1 +1,25 @@
-//TODO
+function townsToJson(towns) {
+  let townsArr = [];
+  for (const town of towns.slice(1)) {
+    const [ignored, townName, latitude, longitude] = town.split(/\s*\|\s*/);
+    const townObj = {
+      Town: townName,
+      Latitude: +latitude,
+      Longitude: +longitude,
+    };
+    townsArr.push(townObj);
+  }
+  return JSON.stringify(townsArr);
+}
+
+console.log(townsToJson([
+  '| Town | Latitude | Longitude |',
+  '| Sofia | 42.696552 | 23.32601 |',
+  '| Beijing | 39.913818 | 116.363625 |',
+]));
+
+console.log(townsToJson([
+  '| Town | Latitude | Longitude |',
+  '| Veliko Turnovo | 43.0757 | 25.6172 |',
+  '| Monatevideo | 34.50 | 56.11 |',
+]));
