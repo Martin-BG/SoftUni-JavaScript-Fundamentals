707775b4e564f1f431b415a51e385b59c5b7f443
MartinBG <MartinBG@abv.bg>
2018-10-07 01:26:29 +0300

    done exercise #3

--- 05. Strings and Regex/Exercise/03. Starts With.js
+++ 05. Strings and Regex/Exercise/03. Starts With.js
@@ -1,1 +1,11 @@
-//TODO
+function startsWith(text, value) {
+  console.log(text.startsWith(value));
+}
+
+startsWith('How have you been?', 'how');
+
+startsWith('The quick brown fox…', 'The quick brown fox…');
+
+startsWith(
+  'Marketing Fundamentals, starting 19/10/2016',
+  'Marketing Fundamentals, sta');
