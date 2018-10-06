66979e9a76d4d7563874e016723ecd05808c31aa
MartinBG <MartinBG@abv.bg>
2018-10-07 01:56:59 +0300

    done exercise #6

--- 05. Strings and Regex/Exercise/06. Capture the Numbers.js
+++ 05. Strings and Regex/Exercise/06. Capture the Numbers.js
@@ -1,1 +1,35 @@
-//TODO
+function captureNumbersRegex(input) {
+  console.log(input
+    .join('')
+    .match(/\d+/g)
+    .join(' ')
+  );
+}
+
+function captureNumbers(input) {
+  console.log(input
+    .join(' ')
+    .split(/\D+/)
+    .filter(x => x !== '')
+    .join(' ')
+  );
+}
+
+captureNumbersRegex([
+  'The300',
+  'What is that?',
+  'I think it’s the 3rd movie.',
+  'Lets watch it at 22:45',
+]);
+
+captureNumbersRegex([
+  '123a456',
+  '789b987',
+  '654c321',
+  '0',
+]);
+
+captureNumbersRegex([
+  'Let’s go11!!!11!',
+  'Okey!1!',
+]);
