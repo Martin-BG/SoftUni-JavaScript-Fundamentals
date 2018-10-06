3defebcd078e2db40aa9f974a703acd7c6a15db4
MartinBG <MartinBG@abv.bg>
2018-10-07 01:44:06 +0300

    done exercise #5

--- 05. Strings and Regex/Exercise/05. Capitalize the Words.js
+++ 05. Strings and Regex/Exercise/05. Capitalize the Words.js
@@ -1,1 +1,23 @@
-//TODO
+function capitalizeWords(text) {
+  console.log(text
+    .toLowerCase()
+    .split(' ')
+    .map((word) => word.charAt(0).toUpperCase() + word.substr(1))
+    .join(' ')
+  );
+}
+
+capitalizeWords('Capitalize these words');
+
+capitalizeWords('Was that Easy? tRY thIs onE for SiZe!');
+
+function capitalizeWordsRegex(str) {
+  console.log(str
+    .toLowerCase()
+    .replace(/^\w|\s\w/g, (letter) => letter.toUpperCase())
+  );
+}
+
+capitalizeWordsRegex('Capitalize these words');
+
+capitalizeWordsRegex('Was that Easy? tRY thIs onE for SiZe!');
