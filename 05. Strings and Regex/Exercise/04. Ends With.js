385c4f0477d338d8d5861602335f5b515910aa97
MartinBG <MartinBG@abv.bg>
2018-10-07 01:29:18 +0300

    done exercise #4

--- 05. Strings and Regex/Exercise/04. Ends With.js
+++ 05. Strings and Regex/Exercise/04. Ends With.js
@@ -1,1 +1,9 @@
-//TODO
+function textEndsWith(text, value) {
+  console.log(text.endsWith(value));
+}
+
+textEndsWith('This sentence ends with fun?', 'fun?');
+
+textEndsWith('This is Houston, we have…', 'We have…');
+
+textEndsWith('The new iPhone has no headphones jack.', 'o headphones jack.');
