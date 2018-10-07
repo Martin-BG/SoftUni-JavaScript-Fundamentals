c8952adc0a299dc9b1f0b833d9519eb334b752ac
MartinBG <MartinBG@abv.bg>
2018-10-07 05:06:19 +0300

    done exercise #5

--- 06. Objects and JSON/Exercise/05. Auto-Engineering Company.js
+++ 06. Objects and JSON/Exercise/05. Auto-Engineering Company.js
@@ -1,1 +1,31 @@
-//TODO
+function autoEngineeringCompany(input) {
+  let cars = new Map();
+  input.forEach(entry => {
+    const [brand, model, count] = entry.split(' | ');
+    if (!cars.has(brand)) {
+      cars.set(brand, new Map());
+    }
+    const previous = cars.get(brand).get(model);
+    const totalProduced = previous ? +count + previous : +count;
+    cars.get(brand).set(model, totalProduced);
+  });
+  cars.forEach((models, brand) => {
+    console.log(brand);
+    models.forEach((produced, model) => {
+      console.log(`###${model} -> ${produced}`);
+    });
+  });
+}
+
+autoEngineeringCompany([
+  'Audi | Q7 | 1000',
+  'Audi | Q6 | 100',
+  'BMW | X5 | 1000',
+  'BMW | X6 | 100',
+  'Citroen | C4 | 123',
+  'Volga | GAZ-24 | 1000000',
+  'Lada | Niva | 1000000',
+  'Lada | Jigula | 1000000',
+  'Citroen | C4 | 22',
+  'Citroen | C5 | 10',
+]);
