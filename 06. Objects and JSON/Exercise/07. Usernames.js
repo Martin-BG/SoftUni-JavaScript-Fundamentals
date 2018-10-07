737bf111e39c08a30ff4fed0d78f697d49d806bc
MartinBG <MartinBG@abv.bg>
2018-10-07 06:51:15 +0300

    done exercise #7

--- 06. Objects and JSON/Exercise/07. Usernames.js
+++ 06. Objects and JSON/Exercise/07. Usernames.js
@@ -1,1 +1,35 @@
-//TODO
+function usernames(input) {
+  let names = new Set();
+  input.forEach(name => names.add(name));
+  console.log([...names]
+    .sort((a, b) => a.length - b.length || a.localeCompare(b))
+    .join('\n')
+  );
+}
+
+usernames([
+  'Ashton',
+  'Kutcher',
+  'Ariel',
+  'Lilly',
+  'Keyden',
+  'Aizen',
+  'Billy',
+  'Braston',
+]);
+
+usernames([
+  'Denise',
+  'Ignatius',
+  'Iris',
+  'Isacc',
+  'Indie',
+  'Dean',
+  'Donatello',
+  'Enfuego',
+  'Benjamin',
+  'Biser',
+  'Bounty',
+  'Renard',
+  'Rot',
+]);
