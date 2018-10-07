9884bf007b04ee0e92675ac9712b4e7d3c46dddd
MartinBG <MartinBG@abv.bg>
2018-10-07 03:45:25 +0300

    done exercise #1

--- 06. Objects and JSON/Exercise/01. Heroic Inventory.js
+++ 06. Objects and JSON/Exercise/01. Heroic Inventory.js
@@ -1,1 +1,20 @@
-//TODO
+function heroicInventory(input) {
+  let heroes = [];
+  input.forEach(hero => {
+    const [name, levelStr, inventory] = hero.split(/\s*\/\s*/g);
+    const items = inventory && inventory.split(/\s*,\s*/g) || [];
+    const level = +levelStr;
+    heroes.push({name, level, items});
+  });
+  console.log(JSON.stringify(heroes));
+}
+
+heroicInventory([
+  'Isacc / 25 / Apple, GravityGun',
+  'Derek / 12 / BarrelVest, DestructionSword',
+  'Hes / 1 / Desolator, Sentinel, Antara',
+]);
+
+heroicInventory(['Jake / 1000 / Gauss, HolidayGrenade']);
+
+heroicInventory(['No Items Guy / 1000 ']);
