2dc2481746f3c951012746ff74e74cf75b2d3084
MartinBG <MartinBG@abv.bg>
2018-10-07 04:22:17 +0300

    done exercise #3

--- 06. Objects and JSON/Exercise/03. Cappy Juice.js
+++ 06. Objects and JSON/Exercise/03. Cappy Juice.js
@@ -1,1 +1,36 @@
-//TODO
+function cappyJuice(input) {
+  const bottleVolume = 1000;
+  let juices = new Map();
+  let bottles = new Set();
+  input.forEach(income => {
+    const [juice, quantity] = income.split(/\s+=>\s+/);
+    if (juices.has(juice)) {
+      juices.set(juice, +quantity + juices.get(juice));
+    } else {
+      juices.set(juice, +quantity);
+    }
+    if (!bottles.has(juice) && juices.get(juice) >= bottleVolume) {
+      bottles.add(juice);
+    }
+  });
+  bottles.forEach(juice => {
+    console.log(`${juice} => ${Math.floor(juices.get(juice) / bottleVolume)}`);
+  });
+}
+
+cappyJuice([
+  'Orange => 2000',
+  'Peach => 1432',
+  'Banana => 450',
+  'Peach => 600',
+  'Strawberry => 549',
+]);
+
+cappyJuice([
+  'Kiwi => 234',
+  'Pear => 2345',
+  'Watermelon => 3456',
+  'Kiwi => 4567',
+  'Pear => 5678',
+  'Watermelon => 6789',
+]);
