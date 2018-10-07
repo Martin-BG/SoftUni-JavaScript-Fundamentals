bbb417b42de4005999f87be297314c61c934a0af
MartinBG <MartinBG@abv.bg>
2018-10-07 03:11:34 +0300

    done exercise #9

--- 05. Strings and Regex/Exercise/09. Extract Links.js
+++ 05. Strings and Regex/Exercise/09. Extract Links.js
@@ -1,1 +1,21 @@
-//TODO
+function extractLinks(input) {
+  console.log((input.join(' ').match(/www\.[a-zA-Z0-9-]+(\.[a-z]+)+/g) || []).join('\n'));
+}
+
+extractLinks([
+  'Join WebStars now for free, at www.web-stars.com',
+  'You can also support our partners:',
+  'Internet - www.internet.com',
+  'WebSpiders - www.webspiders101.com',
+  'Sentinel - www.sentinel.-ko',
+]);
+
+extractLinks([
+  'Need information about cheap hotels in London?',
+  'You can check us at www.london-hotels.co.uk!',
+  'We provide the best services in London.',
+  'Here are some reviews in some blogs:',
+  '"London Hotels are awesome!" - www.indigo.bloggers.com',
+  '"I am very satisfied with their services" - ww.ivan.bg',
+  '"Best Hotel Services!" - www.rebel21.sedecrem.moc',
+]);
